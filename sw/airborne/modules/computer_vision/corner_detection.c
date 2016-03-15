986ff43d8ea0c3ba5b368b67ec805c0f10b96181
wilcoschoneveld <schoneveld.wj@gmail.com>
2016-03-15 17:47:51 +0100

    working lucas kanade

--- sw/airborne/modules/computer_vision/corner_detection.c
+++ sw/airborne/modules/computer_vision/corner_detection.c
@@ -27,6 +27,7 @@
 #include "modules/computer_vision/cv.h"
 #include "modules/computer_vision/corner_detection.h"
 #include "lib/vision/fast_rosten.h"
+#include "lib/vision/lucas_kanade.h"
 
 #include "subsystems/datalink/telemetry.h"
 #include <stdio.h>
@@ -38,24 +39,47 @@ uint16_t min_dist  = 10;
 uint16_t x_padding = 30; // The padding in the x direction to not scan for corners
 uint16_t y_padding = 60; // The padding in the y direction to not scan for corners
 
+// IMG = 272 x 272
+struct image_t img_gray;
+struct image_t img_old;
+
 // Function
 bool_t corner_detection_func(struct image_t* img);
 bool_t corner_detection_func(struct image_t* img)
 {
+  // Storage for feature & vector count
   uint16_t feature_cnt;
 
-  // IMG = 272 x 272
-  image_to_grayscale(img, img);
+  // Convert image to grayscale
+  image_to_grayscale(img, &img_gray);
 
-  struct point_t *features = fast9_detect(img, threshold, min_dist, x_padding, y_padding, &feature_cnt);
+  // Find features to track
+  struct point_t *features = fast9_detect(&img_gray, threshold, min_dist, x_padding, y_padding, &feature_cnt);
 
-  image_show_points(img, features, feature_cnt);
+  // Show fast features found
+//  image_show_points(img, features, feature_cnt);
 
+  // Calculate optical flow from features found
+  struct flow_t *vectors = opticFlowLK(&img_gray, &img_old, features, &feature_cnt, 10, 10, 10, 2, 25);
+
+  // Show optical flow on original image
+  image_show_flow(img, vectors, feature_cnt, 10);
+
+  // Free vector memory
+  free(vectors);
+
+  // Copy new image to old image
+  image_copy(&img_gray, &img_old);
+
+  // TODO: why false?
   return FALSE;
 }
 
 void corner_detection_init(void)
 {
+  image_create(&img_gray, 272, 272, IMAGE_GRAYSCALE);
+  image_create(&img_old, 272, 272, IMAGE_GRAYSCALE);
+
   cv_add(corner_detection_func);
 }
 
